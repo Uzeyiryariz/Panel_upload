505af765afeadec2f784624897100c4fca7b5a6a
uzeyir-yariz <99393019+uzeyir-yariz@users.noreply.github.com>
2023-05-30 20:59:00 +0300

    update for some bug reported

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+var currentPath = window.location.href.replace("index.html", "");
+
 function sign_post(){
     var name = document.getElementById('name').value;
     var password = document.getElementById('password').value;
 
     if(name == "uzeyir-yariz" && password == "aperture"){
         alert("giriş başarılı");
-        window.location.href = "file:///C:/Users/Dev12/Desktop/Panel-main/public/panel.html";
+        window.location.href = currentPath + "public/panel.html";
+
+        // file:///C:/Users/uzeyi/OneDrive/Masa%C3%BCst%C3%BC/projeler/projeler/Panel_upload/index.html
+        // * file:///C:/Users/Dev12/Desktop/Panel-main/public/panel.html
+        // * file:///C:/Users/uzeyi/OneDrive/Masa%C3%BCst%C3%BC/projeler/projeler/Panel_upload/public/panel.html
+
     } else{
         alert("İsim yada şifre yanlış");
     }
 }
+
+alert(currentPath);
