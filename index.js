f690ce0932067ef93e511a0c8ec7697c2c716b04
uzeyir-yariz <uzeyiryariz13@gmail.com>
2023-06-01 08:52:30 +0000

    update

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-analytics.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
+
 var currentPath = window.location.href.replace("index.html", "");
 
-function sign_post(){
+function sign_post() {
+    
+    const firebaseConfig = {
+        apiKey: "AIzaSyBKopaV6A9xnhu0IYxgidd8XMmZiKb0CVc",
+        authDomain: "massive-jet-371518.firebaseapp.com",
+        projectId: "massive-jet-371518",
+        storageBucket: "massive-jet-371518.appspot.com",
+        messagingSenderId: "1008582599309",
+        appId: "1:1008582599309:web:3c8a84cd1145951773f8c4",
+        measurementId: "G-FXW6KBNX2H"
+    };
+            
+    const app = initializeApp(firebaseConfig);
+    // const analytics = getAnalytics(app);    
+
     var name = document.getElementById('name').value;
     var password = document.getElementById('password').value;
 
-    if(name == "uzeyir-yariz" && password == "aperture"){
-        alert("giriş başarılı");
+    signInWithEmailAndPassword(app.auth(), name, password)
+    .then(function(userCredential) {
+        alert("Giriş başarılı");
         window.location.href = currentPath + "public/panel.html";
-
-        // file:///C:/Users/uzeyi/OneDrive/Masa%C3%BCst%C3%BC/projeler/projeler/Panel_upload/index.html
-        // * file:///C:/Users/Dev12/Desktop/Panel-main/public/panel.html
-        // * file:///C:/Users/uzeyi/OneDrive/Masa%C3%BCst%C3%BC/projeler/projeler/Panel_upload/public/panel.html
-
-    } else{
-        alert("İsim yada şifre yanlış");
-    }
+    })
+    .catch(function(error) {
+        alert("İsim ya da şifre yanlış");
+    });
 }
 
-alert(currentPath);
+
+// alert(currentPath);
